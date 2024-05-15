86f6d026cf043187bf10deaab43a899594347910
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-05-15 20:30:38 +0300

    [install] prettier tailwind plugin

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-	<div class="h-screen grid place-items-center">
+	<div class="h-screen m-5 grid place-items-center">
 		<div>
 			<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight mb-10">🎨 Icon Duotoner</h1>
-				<Button variant="outline" class="w-full h-fit p-0">
-					<label
-					class="hover:cursor-pointer w-full h-24 flex items-center justify-center"
-					for="file-uploader">
-					<Upload class="inline me-2 w-4" />
-						Choose an SVG file
-					</label>
-					</Button>
-				<input class="hidden" id="file-uploader" type="file" accept="image/svg+xml" />
+			<label for="file-uploader">
+				<Upload class="inline w-4 me-2" />
+				Choose an SVG file
+			</label>
+			<input class="hidden" id="file-uploader" type="file" accept="image/svg+xml" />
 		</div>
 	</div>
 </template>
 
-
 <script setup>
-import Button from './components/ui/button/Button.vue';
 import { Upload } from 'lucide-vue-next';
 </script>
+
+<style scoped>
+label[for="file-uploader"] {
+	@apply hover:cursor-pointer w-full h-24 flex items-center justify-center border border-slate-200 bg-white shadow-sm hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50 px-4 py-2 rounded-md
+}
+</style>
