b31ab9adab3bbb431c6568208c9811eb1589d066
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-05-15 21:05:14 +0300

    [add] color iput and download button

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,14 @@
 					Choose an SVG file
 				</label>
 				<input class="hidden" id="file-uploader" type="file" accept="image/svg+xml" />
-				<label for="tone-opacity-slider">Secondary Tone Opacity</label>
+				<label for="tone-color-input">Color</label>
+				<input class="h-8 border-0 [&::-webkit-appearance]:hidden" type="color" id="tone-color-input" />
+				<label for="tone-opacity-slider">Tone Opacity</label>
 				<Slider id="tone-opacity-slider" :default-value="[5]" :max="10" />
+				<Button>
+					<Download class="inline me-2 w-4" />
+					Download
+				</Button>
 			</form>
 		</div>
 	</div>
@@ -20,13 +26,24 @@
 <script setup>
 // Icons
 import { Upload } from 'lucide-vue-next';
+import { Download } from 'lucide-vue-next';
 
 // Components
 import Slider from './components/ui/slider/Slider.vue';
+import Button from './components/ui/button/Button.vue';
 </script>
 
 <style scoped>
 label[for='file-uploader'] {
 	@apply flex h-24 w-full items-center justify-center rounded-md border border-slate-200 bg-white px-4 py-2 shadow-sm hover:cursor-pointer hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50;
+}
+
+input[type="color"]::-webkit-color-swatch-wrapper {
+	padding: 0;
+}
+
+input[type="color"]::-webkit-color-swatch {
+	border: none;
+	@apply rounded-md;
 }
 </style>
