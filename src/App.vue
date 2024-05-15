6616d5434dde06a058feb227f8ea21ed919bc6a1
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-05-15 21:20:48 +0300

    [style] layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-	<div class="m-5 grid h-screen place-items-center">
-		<div>
-			<h1 class="mb-10 scroll-m-20 text-4xl font-extrabold tracking-tight">
-				🎨 Icon Duotoner
-			</h1>
+	<div class="m-5 sm:m-0 w-screen flex flex-col md:flex-row h-screen">
+		<!-- Options Pane -->
+		<div class="w-1/3 grid place-items-center">
 			<form class="flex flex-col gap-4">
+				<h1 class="mb-5 text-center scroll-m-20 text-4xl font-extrabold tracking-tight">
+					🎨 Icon Duotoner
+				</h1>
 				<label for="file-uploader">
 					<Upload class="inline me-2 w-4" />
 					Choose an SVG file
@@ -20,6 +21,10 @@
 				</Button>
 			</form>
 		</div>
+		<!-- Preview Pane -->
+		<div class="w-2/3 grid place-items-center">
+			Preview Here
+		</div>
 	</div>
 </template>
 
@@ -34,16 +39,20 @@ import Button from './components/ui/button/Button.vue';
 </script>
 
 <style scoped>
-label[for='file-uploader'] {
-	@apply flex h-24 w-full items-center justify-center rounded-md border border-slate-200 bg-white px-4 py-2 shadow-sm hover:cursor-pointer hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50;
-}
+	/* * {
+		border: 1px solid red; 
+	} */
 
-input[type="color"]::-webkit-color-swatch-wrapper {
-	padding: 0;
-}
+	label[for='file-uploader'] {
+		@apply flex h-24 w-full items-center justify-center rounded-md border border-slate-200 bg-white px-4 py-2 shadow-sm hover:cursor-pointer hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50;
+	}
 
-input[type="color"]::-webkit-color-swatch {
-	border: none;
-	@apply rounded-md;
-}
+	input[type="color"]::-webkit-color-swatch-wrapper {
+		padding: 0;
+	}
+
+	input[type="color"]::-webkit-color-swatch {
+		border: none;
+		@apply rounded-md;
+	}
 </style>
